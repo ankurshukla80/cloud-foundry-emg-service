70458b5299e795526530877fedc73f64904cccc0
shuklaankur <ankur.shukla@gmail.com>
2019-10-23 14:05:05 -0700

    Removed extra debug statements

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # cloud-foundry-emg-service
-This repo
+This repo contains code for the EMG Service Plugin that facilitates cloud foundry route services based integration within a User Provided Service Pattern (no Service Broker involved).
--- index.js
+++ index.js
@@ -4,51 +4,18 @@ var url = require('url');
 
 module.exports.init = function(config, logger, stats) {
   
-  // perform content transformation here
-  // the result of the transformation must be another Buffer
-  function transform(data) {
-    return new Buffer(data.toString().toUpperCase());
-  }
   return {
    
     onrequest: function(req, res, data, next) {
-      console.log('***** EMG Service plugin - event - onrequest');
-      console.log('original req.targetHostname : '+ JSON.stringify(req.targetHostname));
-      console.log('original req.targetPath : '+ JSON.stringify(req.targetPath));
 
       var adr = req.headers['x-cf-forwarded-url'];
       var addressParsed = url.parse(adr, true);
       
-      console.log(addressParsed.host); //returns 'localhost:8080'
-      console.log(addressParsed.pathname); //returns '/default.htm'
       req.targetHostname = addressParsed.host;
       console.log('New req.targetHostname : '+ JSON.stringify(req.targetHostname));
       req.targetPath = addressParsed.pathname;
       console.log('New req.targetPath : '+ JSON.stringify(req.targetPath));
       next(null, data);
-    },
-    // chunk of request body data received from client
-    // should return (potentially) transformed data for next plugin in chain
-    // the returned value from the last plugin in the chain is written to the target
-    ondata_response: function(req, res, data, next) {
-      // transform each chunk as it is received
-      next(null, data ? transform(data) : null);
-    },
-
-    onend_response: function(req, res, data, next) {
-      // transform accumulated data, if any
-      next(null, data ? transform(data) : null);
-    },
-
-    ondata_request: function(req, res, data, next) {
-      // transform each chunk as it is received
-      next(null, data ? transform(data) : null);
-    },
-
-    onend_request: function(req, res, data, next) {
-      // transform accumulated data, if any
-      next(null, data ? transform(data) : null);
     }
-    
   };
 }
