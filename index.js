3ac01d2eaa4a8678a14b250f92d9b3541c3b448c
shuklaankur <ankur.shukla@gmail.com>
2019-10-23 17:03:36 -0700

    Added error handling when no cf headers are present

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ module.exports.init = function(config, logger, stats) {
     onrequest: function(req, res, data, next) {
 
       var adr = req.headers['x-cf-forwarded-url'];
-      var addressParsed = url.parse(adr, true);
+      console.log("Adr"+ adr);
+      if(adr != null)
+      {
+        var addressParsed = url.parse(adr, true);
       
-      req.targetHostname = addressParsed.host;
-      console.log('New req.targetHostname : '+ JSON.stringify(req.targetHostname));
-      req.targetPath = addressParsed.pathname;
-      console.log('New req.targetPath : '+ JSON.stringify(req.targetPath));
+        req.targetHostname = addressParsed.host;
+        console.log('New req.targetHostname : '+ JSON.stringify(req.targetHostname));
+        req.targetPath = addressParsed.pathname;
+        console.log('New req.targetPath : '+ JSON.stringify(req.targetPath));
+      }
       next(null, data);
     }
   };
